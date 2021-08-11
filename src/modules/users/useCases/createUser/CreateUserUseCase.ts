d95e907e19fbb6f1874ca6c1303a9967bd6e39c7
fbsoares-lu <lsoareshfb@gmail.com>
2021-08-10 21:36:34 -0300

    Resolve the challenge

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userExists = this.usersRepository.findByEmail(email);
+
+        if (userExists) {
+            throw new Error("User email alredy exists");
+        }
+
+        const user = this.usersRepository.create({ name, email });
+
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
